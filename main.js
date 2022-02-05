8b9ea34c1b9bd44526fa05ece7153b4711f96e3b
Collo <mutaic84@gmail.com>
2022-02-05 10:40:49 +0300

    Update the progress bas as questions get answered

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const question = document.getElementById('question');
 const choices  = Array.from(document.getElementsByClassName('choice-text'));
-const questionCounterText = document.getElementById('questionCounter');
+const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
-
+const progressBarFull = document.getElementById('progressBarFull');
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -48,7 +48,10 @@ getNewQuestion = () => {
     }
     questionCounter++;
 
-    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+    // update progress bar
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`;
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
