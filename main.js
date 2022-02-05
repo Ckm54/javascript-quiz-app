ace5336242df5d48720b721b370805a93a1e4fda
Collo <mutaic84@gmail.com>
2022-02-05 08:54:48 +0300

    Load new questions and choices available to quizes page

---
+++ main.js
@@ -0,0 +1,77 @@
+const question = document.getElementById('question');
+const choices  = Array.from(document.getElementsByClassName('choice-text'));
+
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+    {
+        question : "Inside which html element do we put javascript?",
+        choice1 : "<script>",
+        choice2 : "<javascript>",
+        choice3 : "<js>",
+        choice4 : "<scripting>",
+        answer : 1,
+    },
+    {
+        question: "What is the correct syntax for referring to an external script named 'script.js'?",
+        choice1 : "<script href='script.js'>",
+        choice2 : "<script name='script.js'>",
+        choice3 : "<script src='script.js'>",
+        choice4 : "<script file='script.js'>",
+        answer : 3,
+    },
+]
+
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+startquiz = () => {
+    questionCounter = 0;
+    score = 0;
+    // do a full copy of all questions from the questions array
+    availableQuestions = [...questions];
+    console.log(availableQuestions);
+    getNewQuestion();
+}
+
+getNewQuestion = () => {
+
+    if (availableQuestions.length == 0 || questionCounter > MAX_QUESTIONS) {
+        // GO TO THE END PAGE
+        return window.location.assign('/end.html')
+    }
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach( choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+
+    availableQuestions.splice(questionIndex, 1);
+
+    acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
+
+        getNewQuestion();
+    })
+})
+
+
+startquiz()
